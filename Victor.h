2a03ae9b725fdc0f1c36b5ac4847d8b2e5beb1a6
Alexey <113046879+NeymanPros@users.noreply.github.com>
2022-12-13 00:58:34 +0300

    Add files via upload

--- Victor.h
+++ Victor.h
@@ -1,11 +1,4 @@
-#include <ostream>
 #include <cmath>
-#include <vector>
-
-struct cords{
-    double x;
-    double y;
-};
 
 struct nt{
     double x = 0;
@@ -17,37 +10,55 @@ struct pol{
     double len = 0;
 };
 
-struct answer{
-    double sc = 0;
-    nt sumnt = {0, 0};
-    pol sumpol = {0, 0};
-};
+void ntToPol(const nt &a, pol &b) {
+    if(sqrt(a.x * a.x + a.y * a.y) < 0.0001){
+        std::cerr << "Object has fallen!\n";
+        return;
+    }
 
-
-void ntToPol(const nt &a, pol &b){
     b.len = sqrt(a.x * a.x + a.y * a.y);
     b.alfa = asin(a.y / b.len);
+    if(a.x < 0){
+        b.alfa = (a.y > 0 ? M_PI - b.alfa : -M_PI - b.alfa);
+    }
+}
+
+void ntToAlfa(const nt &a, pol &b) {
+    if(sqrt(a.x * a.x + a.y * a.y) < 0.0001){
+        std::cerr << "Object has fallen!\n";
+        return;
+    }
+
+    b.alfa = asin(a.y / sqrt(a.x * a.x + a.y * a.y));
+    if(a.x < 0){
+        b.alfa = (a.y > 0 ? M_PI - b.alfa : -M_PI - b.alfa);
+    }
+}
+
+void PolToNt(nt &a, const pol &b) {
+    a.x = b.len * cos(b.alfa);
+    a.y = b.len * sin(b.alfa);
+}
+
+double scalar(const nt &a, const nt &b){
+    return (a.x * b.x + a.y * b.y);
+}
+
+double scalar(const pol &a, const pol &b) {
+    return (a.len * b.len * cos(b.alfa - a.alfa));
 }
 
 
-void scalar(const nt &a, const nt &b, std::vector <answer> &ans, const int &i){
-    ans.resize(ans.size() + 1);
-    ans[i].sc = a.x * b.x + a.y * b.y;
-    std::cout << a.x * b.x + a.y * b.y << "\n";
-}
-
-void scalar(const pol &a, const pol &b) {
-    std::cout << a.len * b.len * cos(a.alfa - b.alfa) << "\n";
-}
-
-
-answer sum(const nt &a, const nt &b, std::vector <answer> &ans, const int &i) {
-    ans[i].sumnt = {a.x + b.x, a.y + b.y};
-    std::cout << a.x + b.x << " " << a.y + b.y << "\n";
+nt sum(const nt &a, const nt &b) {
     return {a.x + b.x, a.y + b.y};
 }
 
-void sum(const pol &a, const pol &b, std::vector <answer> &ans, const int &i){
-    a.len - b.len * (a.alfa / b.alfa);
-    std::cout << " ";
+pol sum(const pol &a, const pol &b){
+    nt ua, ub;
+    pol ans;
+    PolToNt(ua, a);
+    PolToNt(ub, b);
+    ua = sum(ua, ub);
+    ntToPol(ua, ans);
+    return ans;
 }
--- main.cpp
+++ main.cpp
@@ -2,25 +2,24 @@
 #include "Victor.h"
 
 int main() {
-    std::cout.precision(3);
-    cords m, c;
-    double ms = 0, mr = 0, cs = 0, cr = 0, add = 0;
-    cords cv = {0, 0};
-    std::cout << "x, y, speed, r\n";
-    std::cin >> m.x >> m.y >> ms >> mr >> c.x >> c.y >> cs >> cr;
-    double xa = c.x;
+    std::cout.precision(4);
+    double dist = 0;
+    nt a = {0, 5}, skor = {1.3, 0}, dvi;
+    pol acc = {1, 1}, speed, mov;
 
-    for(int i = 10; i > 0; i--){
-        add = (xa - m.x)/i;
-        m.x += add;
-        cv.x = m.x - c.x;
-        cv.y = (m.y - c.y);
-        c.x += cv.x/i*cs/ms;
-        c.y += cv.y/i*cs/ms;
+    //std::cin >> a.x >> a.y >> skor.x >> skor.y >> acc.len;
+    acc.len = -acc.len;
 
-        std::cout << "\t" << m.x << "\t" << m.y << "\t" << c.x << "\t" << c.y << "\n";
+    for (int t = 0; t < 50; t++) {
+        dist = abs(a.x * a.x + a.y * a.y);
+        ntToAlfa(a, acc);
+        ntToPol(skor, speed);
+        mov = sum(speed, {acc.alfa, acc.len/dist});
+        PolToNt(dvi, mov);
+        a.x += dvi.x / 10;
+        a.y += dvi.y / 10;
+        skor.x = dvi.x;
+        skor.y = dvi.y;
+        std::cout << a.x << "\t" << a.y << "\n";
     }
 }
-/*m.x = (m.x < 0.0001 ? 0 : m.x);
-cv.x = (cv.x < 0.0001 ? 0 : cv.x);
-cv.y = (cv.y < 0.0001 ? 0 : cv.y);*/
